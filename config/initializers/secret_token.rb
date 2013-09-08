3879cb2dcef6d75f8a2b750f45b759ede73e06a3
Jukka Hyytiälä <hyytiala.jukka@gmail.com>
2013-09-08 16:51:30 +0300

    Started using Rails::API

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 
 # Make sure the secret is at least 30 characters and all random,
@@ -9,4 +9,11 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SignagePi::Application.config.secret_key_base = 'c8f8a9ca40530ecbaf29791af08f1499d4a6be0ee746b21fe403e5429e116feb93703e59764a447fc58091b2eb8d98bd98d5282c7201cb2dc35d1f6870e5f801'
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_toke to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+SignagePi::Application.config.secret_token = '85cda5204f945549840caa7fd384144efa36183ad131f2f09b65885491129c98e8c7c3c39e08c951310acbcc09e4d657fa6d4e97e04907aa114987320eee8d2c'
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-SignagePi::Application.config.session_store :cookie_store, key: '_signage-pi_session'
